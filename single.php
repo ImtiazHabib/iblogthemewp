7cb121eabb54451e3bae9260cc8200b25c40600a
ImtiazHabib <imtiazhabib7@gmail.com>
2024-05-04 00:54:42 +0600

    single post view create

--- single.php
+++ single.php
@@ -7,45 +7,46 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <h3 class="tagline">
+                <a href="http://localhost/ithemeone/about/">
+                   <h3 class="tagline">
                     <?php echo bloginfo( "name" ); ?>
-                </h3>
+                   </h3> 
+                </a>
+                
                 <h1 class="align-self-center display-1 text-center heading"><?php bloginfo("description"); ?></h1>
             </div>
         </div>
     </div>
 </div>
-<div class="posts" >
+<div class="posts <?php post_class(  ); ?>" >
     <!-- post started -->
     <?php
     while (have_posts()) {
         the_post(  );
         ?>
-        <div class="post" <?php post_class(  ); ?> >
         <div class="container">
             <div class="row">
-                <div class="col-md-12">
-                <a href="<?php the_permalink( ); ?>">    
-                 <h2 class="post-title">
+                <div class="col-md-10 offset-md-1">   
+                 <h2 class="post-title text_decoration_single">
                         <?php
                             the_title(  );
                         ?>
-                    </h2>
-                </a>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-4">
-                    <p>
+                 </h2>
+              
+                <p class="text_decoration_single">
                         <strong>
                         <?php
                             the_author(  );
                         ?> 
                         </strong><br/>
                         <?php
-                         the_date(); 
+                         echo get_the_date(); 
                         ?>
                     </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-10 offset-md-1">
                     <!-- <ul class="list-unstyled">
                         <li>dhaka</li>
                     </ul> -->
@@ -53,10 +54,7 @@
                     <!-- showing post tags start -->
                    <?php echo get_the_tag_list( "<ul class=\"list-unstyled\"><li>", "<li></li>","</li></ul>"); ?>
                     <!-- showing post tags end -->
-
-                </div>
-                <div class="col-md-8">
-                    <p>
+                     <p>
                         <?php
                         if(has_post_thumbnail(  )){
                             the_post_thumbnail( "large", array("class"=>"img-fluid") );
@@ -68,22 +66,25 @@
                         <?php
 
                            
-                            the_excerpt();
+                             the_content(  );
                            
                         ?>
                         <!-- checking single page or not end -->
 
                         
                     </p>
+
                 </div>
+                
             </div>
 
         </div>
-    </div>
+    
         <?php
     }
     ?>
     <!-- post end -->  
+</div>
     <div class="posts_pagination">
         <div class="container">
             <div class="row">
